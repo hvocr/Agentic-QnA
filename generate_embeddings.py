19080e3f058ecb681315be98d8ea2bfb1ca333a2
SannihithVikramJ <moron4gyy@gmail.com>
2026-07-30 12:13:08 +0530

    Add embedding generation functionality with Streamlit

---
+++ generate_embeddings.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+import streamlit as st
+
+@st.cache_resource
+def get_embedding_model():
+    return SentenceTransformer('all-mpnet-base-v2')
+
+def generate_embeddings(chunks: list):
+    model = get_embedding_model()
+    return model.encode(chunks, convert_to_numpy=True)
